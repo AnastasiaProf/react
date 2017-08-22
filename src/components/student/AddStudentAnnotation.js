5f28fba71111d6877160f68656eab9ea0f460deb
Anastasia Chinsky <anastasia.chinsky@isep.fr>
2017-08-22 09:16:48 +0800

    tags start

--- src/components/student/AddStudentAnnotation.js
+++ src/components/student/AddStudentAnnotation.js
@@ -10,8 +10,7 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import Panel from 'react-bootstrap/lib/Panel';
 import Button from 'react-bootstrap/lib/Button';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import Checkbox from 'react-bootstrap/lib/Checkbox';
+import TokenAutocomplete from 'react-token-autocomplete';
 import getStudentInfo from '../../queries/fetchAnnotations';
 import CourseStudentQuery from '../../queries/fetchStudentsList'
 
@@ -27,10 +26,7 @@ class AddStudentAnnotation extends Component{
             text: '',
             studentIDs: [props.studentID],
             teacherID: props.teacherID,
-            strength: '',
-            weakness: '',
-            action: '',
-            parent: '',
+            defaultTags:[],
 
         };
     }
@@ -42,25 +38,8 @@ class AddStudentAnnotation extends Component{
         let studentID = this.props.studentID;
         let teacherID = this.props.teacherID;
         let course = this.props.courseID;
-        let tags = [];
-
-        if(!(this.state.strength) == ""){
-            tags.push("Strengths")
-        }
-
-        if(!(this.state.weakness) == ""){
-            tags.push("Weaknesses")
-        }
-
-        if(!(this.state.action) == ""){
-            tags.push("Action Plan")
-        }
-
-        if(!(this.state.parent) == ""){
-            tags.push("Parent update")
-        }
-
-
+        let defaultTags = this.props.defaultTags;
+        
         let date = new Date();
         let local = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "T" +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()+"."+date.getMilliseconds()+"Z";
 
@@ -71,7 +50,7 @@ class AddStudentAnnotation extends Component{
                     text: this.state.text,
                     teacherID: teacherID,
                     studentIDs: [studentID],
-                    tags: tags,
+                    tags: defaultTags,
                     deleted: false,
                     courseID: course,
                     //localCreatedAt : local,
@@ -88,41 +67,6 @@ class AddStudentAnnotation extends Component{
         }).then(() => this.setState({text: ''}));
     }
 
-    //Handle strength checkbox change
-    handleChangeStrength(){
-        if(this.state.strength == ""){
-            this.setState({strength: "on"})
-        } else {
-            this.setState({strength: ""})
-        }
-    }
-
-    //Handle weakness checkbox change
-    handleChangeWeakness(){
-        if(this.state.weakness == ""){
-            this.setState({weakness: "on"})
-        } else {
-            this.setState({weakness: ""})
-        }
-    }
-
-    //Handle action checkbox change
-    handleChangeAction(){
-        if(this.state.action == ""){
-            this.setState({action: "on"})
-        } else {
-            this.setState({action: ""})
-        }
-    }
-
-    //Handle parent checkbox change
-    handleChangeParent(){
-        if(this.state.parent == ""){
-            this.setState({parent: "on"})
-        } else {
-            this.setState({parent: ""})
-        }
-    }
 
     render(){
         return(
@@ -132,23 +76,12 @@ class AddStudentAnnotation extends Component{
                     <form onSubmit={this.onSubmit.bind(this)}>
                         <textarea spellCheck="true" className="students" value= {this.state.text} onChange={event => this.setState({ text: event.target.value})}/>
                         <div className="formsubmit">
-                            <FormGroup className="tags">
-                                <Checkbox onChange={this.handleChangeStrength.bind(this)} value={this.state.strength_value} inline>
-                                    Strengths
-                                </Checkbox >
-                                {' '}
-                                <Checkbox onChange={this.handleChangeWeakness.bind(this)} value={this.state.weakness_value} inline>
-                                    Weaknesses
-                                </Checkbox>
-                                {' '}
-                                <Checkbox onChange={this.handleChangeAction.bind(this)} value={this.state.action_value} inline>
-                                    Action plan
-                                </Checkbox>
-                                {' '}
-                                <Checkbox onChange={this.handleChangeParent.bind(this)} value={this.state.parent_value} inline>
-                                    Parent update
-                                </Checkbox>
-                            </FormGroup>
+                            <TokenAutocomplete
+                                placeholder="type to limit suggestions"
+                                limitToOptions={true}
+                                defaultValues={['apple']}
+                                options={['apple', 'banana', 'carrot', 'watermelon']}/>
+
                             <Button className="submit" type="submit">Submit</Button>
                         </div>
                     </form>
--- src/queries/fetchAnnotations.js
+++ src/queries/fetchAnnotations.js
@@ -36,6 +36,9 @@ const getStudentInfo = gql`
             transcribedAt
             deleted
         }
+        annotationTags{
+            defaultTags
+        }
     }
 `;
 
